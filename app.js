b22d01c669e5ad833457e73511ac0f65998619d7
Md-Asikur <asikurrahaman997@gamil.com>
2022-11-21 03:36:37 -0800

    oi

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config({ path: "./config.env" })
+const PORT=process.env.PORT||7000
 const app = express()
 app.get("/", (req, res) => {
     res.json({Message:"hello world"})
 })
-app.listen(6000, ()=> {
-    console.log("server startted")
+app.listen(PORT, ()=> {
+    console.log(`SERVER IS START AT:${PORT}`)
 })
